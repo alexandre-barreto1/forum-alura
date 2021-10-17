8079247f8e9aed16bd9876fe8ed06844ef1c0b5b
alexandre-dev <alexandre-dev@alexandre-dev>
2021-10-16 23:28:51 -0300

    Adicionando spring secuirity

--- src/main/java/br/com/alura/forum/controller/AluraForumTopicosController.java
+++ src/main/java/br/com/alura/forum/controller/AluraForumTopicosController.java
@@ -8,6 +8,7 @@ import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -99,6 +100,7 @@ public class AluraForumTopicosController {
 
 	@PostMapping
 	@Transactional
+	@CacheEvict(value = "listaDeTopicos", allEntries = true)
 	public ResponseEntity<TopicoDTO> cadastrar(@RequestBody @Valid TopicoForm form, UriComponentsBuilder uriBuilder) {	
 		Topico topico = form.converter(cursoRepository);
 		topicoRepository.save(topico);
@@ -120,6 +122,7 @@ public class AluraForumTopicosController {
 	
 	@PutMapping("/{id}")
 	@Transactional
+	@CacheEvict(value = "listaDeTopicos", allEntries = true)
 	public ResponseEntity<TopicoDTO> atualizar(@PathVariable Long id, @RequestBody @Valid AtualizacaoTopicoForm form){
 		Optional<Topico> topico = topicoRepository.findById(id);
 		if(topico.isPresent()) {
@@ -130,6 +133,7 @@ public class AluraForumTopicosController {
 	
 	@DeleteMapping("/{id}")
 	@Transactional
+	@CacheEvict(value = "listaDeTopicos", allEntries = true)
 	public ResponseEntity deletar(@PathVariable Long id) {
 		Optional<Topico> topico = topicoRepository.findById(id);
 		if(topico.isPresent()) {
